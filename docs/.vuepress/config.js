160abfcfced9167c72f922a4e63910d6627da52f
yinchun.liu <1101032620@qq.com>
2020-06-16 15:07:44 +0800

    feat: 新项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: '丝路 - 电商资讯',
+    description:
+        '卖家之家是国内领先的出口跨境电商卖家服务平台，为卖家提供最新跨境电商资讯、跨境电商运营工具以及测评黑名单，从而为电跨境电商卖家带来卓越用户体验。',
+    head: [['link', { rel: 'icon', href: '/public/img/favicon.ico' }]],
+    themeConfig: {
+        nav: [
+            {text: '电商资讯', link: '/guide/News'}
+        ]
+    },
+};
