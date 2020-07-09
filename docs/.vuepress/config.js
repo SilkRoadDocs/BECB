cd33638604c2c8f474147ed79ec984804802c7e7
Jamestone <zzzhangtong@gmail.com>
2020-07-09 19:45:54 +0800

    7.9日版本

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,10 +8,19 @@ module.exports = {
     themeConfig: {
         nav: [
             {text: '电商资讯', link: '/guide/News'},
-            {text: '丝路', link: 'https://slr.mobvista.com'},
+            {text: '建站', link: '/web_builder/WebBuilder'},
+            {text: '丝', link: 'https://slr.mobvista.com'},
         ],
         sidebar: [
-            '/'
+            '/industry_chain/WebBuilder.md',
+            '/industry_chain/Selection',
+            '/industry_chain/Source',
+            '/industry_chain/Marketing',
+            '/industry_chain/Operation',
+            '/industry_chain/Supplier',
+            '/industry_chain/Logistics',
+            '/industry_chain/Brand',
+            
         ],
     },
 };
--- docs/guide/News.md
+++ docs/guide/News.md
@@ -7,6 +7,3 @@ Sometimes it's useful to have different levels of headings to structure your doc
 You can use one `#` all the way up to `######` six for different heading sizes.
 
 If you'd like to quote someone, use the > character before the line:
-
-> Coffee. The finest organic suspension ever devised... I beat the Borg with it.
-> - Captain Janeway
